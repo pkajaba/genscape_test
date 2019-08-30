e0122085c6f7489ef55a749d95d1fdcc67d0fbb5
Pavel Kajaba <pavel.kajaba@gmail.com>
2019-08-30 11:32:22 +0100

    Add script to build images

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker build -f Dockerfile.storage -t image/storage .  
+docker build -f Dockerfile.ingestion -t image/ingestion .  
+docker build -f Dockerfile.sensor -t image/sensor .  
